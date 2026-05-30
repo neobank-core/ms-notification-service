eef3a157f2d4fa6362c3ef7aa4ff6839bb96c640
Abbashatred <gitabbas26@gmail.com>
2026-05-30 23:12:27 +0400

    first commit

---
+++ src/main/java/com/neobank/notificationservice/MsNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.neobank.notificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsNotificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neobank/notificationservice/MsNotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neobank.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
